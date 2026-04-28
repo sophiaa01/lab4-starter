d2e7de50193175f817b9140ca545d5a3a1796df0
sophiaa01 <alisophia019@gmail.com>
2026-04-28 16:26:18 -0700

    fix buggy code

--- scripts/lab4.js
+++ scripts/lab4.js
@@ -7,14 +7,14 @@
  */
 function sumValues(num1, num2, add) {
     if (add) {
-        const result = 0;
+        let result = 0;
 
         result = num1 + num2;
 
         return result;
     }
     else {
-        return !add;
+        return add;
     }
 }
 
@@ -25,15 +25,34 @@ function sumValues(num1, num2, add) {
  * @returns An array of each price's new price, after the discount is applied. Or false, if prices array is empty.
  */
 function discountPrices(prices, discount) {
-    const discounted = []
+    let discounted = [];
     const length = prices.length;
-    let discountedPrice = 0
-    for(let i = 0; i < length; i++) {
-        discountedPrice += prices[i] * (1 - discount);
-        discounted.push(discountedPrice);
+    let discountedPrice = 0;
+
+    if (discount == 0) {
+        return prices;
     }
 
-    return discounted;
+    else if (discount == 1) {
+        for(let i = 0; i < length; i++) {
+            discounted.push(0);
+        }
+        return discounted;
+    }
+
+    else if (length == 0 || (typeof discount != Number)) {
+        return false;
+    }
+    else {
+        for (let i = 0; i < length; i++) {
+            discountedPrice += prices[i] * (1 - discount);
+            discounted.push(discountedPrice);
+        }
+
+        return discounted;
+    }
 }
 
 module.exports = {sumValues, discountPrices};
+
+// console.log(discountPrices('i\'m not an array', 0.5));
